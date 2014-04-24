a0dabfab2af56491915071aa0260ab42db3e4a3f
wontaeyang <wontaeyang@gmail.com>
2014-04-24 19:59:36 -0400

    changed new jem form in to tabs

---
+++ app/assets/javascripts/new.js
@@ -0,0 +1,9 @@
+
+
+$(document).ready(function() {
+
+  $('#new-form-next').on('click', function(){
+    $('#info-user-tab').removeClass('active')
+    $('#info-gem-tab').addClass('active')
+  });
+});
