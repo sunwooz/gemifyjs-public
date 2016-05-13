c785df63feda9502a1119f5697bf8ee6d4733827
sunwooz <yangsunwoo@gmail.com>
2016-05-13 15:57:06 -0400

    clean routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,10 @@ Rails.application.routes.draw do
   end
 
   devise_for :users, :controllers => {:omniauth_callbacks => 'users/omniauth_callbacks'}
-  devise_scope :user do 
-    delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  devise_scope :user do
+    get '/auth/github/callback' => 'sessions#create', as: :github_callback
+    get '/auth/github' => 'auth#github', as: 'github_login'
+    delete 'sign_out' => 'devise/sessions#destroy', as: :destroy_user_session
   end
 
   root to: 'static#index'
@@ -24,68 +26,10 @@ Rails.application.routes.draw do
   get 'jems/:id/scripts/:script_id' => 'scripts#show', as: 'script'
   delete 'script/:id' => 'scripts#destroy', as: 'delete_script'
 
-  get '/auth/github' => 'auth#github', as: 'github_login'
-  get '/auth/github/callback' => 'sessions#create', as: 'github_callback'
-
-
   post 'create_gem' => 'jems#gemify_jem', as: 'jemify_gem'
   post 'update_gem' => 'jems#gemify_updated_jem'
   get 'percentage_done' => 'jems#percentage_done', as: 'percentage_done'
   get 'get_gem_repo' => 'jems#get_gem_repo'
   post 'update_version' => 'jems#update_version'
 
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 end
