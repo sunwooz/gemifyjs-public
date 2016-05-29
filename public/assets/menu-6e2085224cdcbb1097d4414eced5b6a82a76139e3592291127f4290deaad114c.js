ba6ae2925080aa0c37c009c6a46d89e4fbd5bb72
sunwooz <yangsunwoo@gmail.com>
2016-05-29 17:33:00 -0400

    precompile

---
+++ public/assets/menu-6e2085224cdcbb1097d4414eced5b6a82a76139e3592291127f4290deaad114c.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$("#menu-close").click(function(e){e.preventDefault(),$("#sidebar-wrapper").toggleClass("active")}),$("#menu-toggle").click(function(e){e.preventDefault(),$("#sidebar-wrapper").toggleClass("active")})});
